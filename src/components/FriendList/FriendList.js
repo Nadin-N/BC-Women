8c24e75ae39dc55f80083638a3b3e2d093e3a207
Mariia Akymenko <dafnarianna@gmail.com>
2023-02-13 14:56:34 +0200

    save to localStorage

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,6 +1,31 @@
 import { Component } from 'react';
+import storage from 'helpers/storage';
 import { FriendForm } from '../FriendForm/FriendForm';
 import { nanoid } from 'nanoid';
+
+const FRIENDS = [
+  {
+    name: 'Мария',
+    lastname: 'Акименко',
+    email: 'dafnarianna@gmail.com',
+    number: '+30565',
+    id: 'POi8Jktn9YUPC5wkkyovs',
+  },
+  {
+    name: 'fdsdfsddsfs',
+    lastname: 'Акименко',
+    email: 'dafnarianna@gmail.com',
+    number: 'dafnarianna@gmail.com',
+    id: '0Hp1-0Asd5qcnmjjV-bLJ',
+  },
+  {
+    name: 'fdgfgfjnfg',
+    lastname: 'Акименко',
+    email: 'dafnarianna@gmail.com',
+    number: 'dafnarianna@gmail.com',
+    id: '8iv5F6-LOdo_CiiIQefef',
+  },
+];
 
 export class FriendList extends Component {
   state = {
@@ -8,18 +33,30 @@ export class FriendList extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const savedFriends = storage.load('friends-list') ?? FRIENDS;
+    this.setState({ friends: savedFriends });
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { friends } = this.state;
+    if (prevState.friends !== friends) {
+      storage.save('friends-list', friends);
+      console.log(friends);
+    }
+  }
+
   addFfiend = friend => {
-   const isExist = this.state.friends.find(({name})=>{
-    return friend.name === name
-   })
-   if(isExist){
-        alert('This friend is existed!!!!'); return
-   }
+    const isExist = this.state.friends.find(({ name }) => {
+      return friend.name === name;
+    });
+    if (isExist) {
+      alert('This friend is existed!!!!');
+      return;
+    }
     this.setState(prevState => {
       return { friends: [...prevState.friends, { ...friend, id: nanoid() }] };
     });
-
-    
   };
   handleFilter = ({ target: { value } }) => {
     this.setState({ filter: value });
@@ -52,7 +89,7 @@ export class FriendList extends Component {
         {friends.length > 0 && (
           <ul>
             {filteredFriends.map(({ name, lastname, email, number, id }) => {
-                return (
+              return (
                 <li key={name}>
                   <span>Name: {name}</span>
                   <span>Surname: {lastname}</span>
