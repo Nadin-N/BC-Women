3748aeee41f292b242c83a58bcbb5fac0c548941
Daria Hryshchenko <daria199210@gmail.com>
2023-02-22 15:38:11 +0200

    redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
+import { Provider } from 'react-redux';
 import { Global, ThemeProvider } from '@emotion/react';
 import { GlobalStyles } from 'styles/GlobalStyles';
 import 'modern-normalize/modern-normalize.css';
 import { theme } from './styles/theme';
 // import './index.css';
+import { store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <Global styles={GlobalStyles} />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <Global styles={GlobalStyles} />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
